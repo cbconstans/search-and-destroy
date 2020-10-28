350f43f281de0f7f1749c37a6cd5f4267f3e3622
Claire Brown <claireconstans@msn.com>
2020-10-28 14:59:01 -0700

    End of pair work

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,6 +2,24 @@
 
 // Complete this algo
 const minJumps = arr => {
+  // if array.length < array[0] to check for base case
+
+  if (arr.length - 1 <= arr[0]) return 1
+
+  // let count = 0 , currentIndexValue = 0
+  // while loop (currentIndex < arr.length) 
+    // jump to another index 
+    // for loop (let i = currentIndexValue, i > 0; i--)
+    // for (let i = currentIndex; i < currentIndex + currentIndex Value; i++) -- LEFT OFF HERE
+
+      //check array value of currentIndex + i
+        //is this the largest value in the for loop?
+    //currentIndexValue += arr[currentIndexValue]
+
+    //increase count
+
+    //return count
+
 
 };
 
